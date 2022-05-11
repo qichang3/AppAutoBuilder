8ad19e40f48f0f9ffb077fd441033084499fb898
Achilles0321 <74133347+Achilles0321@users.noreply.github.com>
2022-05-11 14:40:47 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -4,6 +4,60 @@
 import os
 
 user_home = os.environ['HOME'] # env user_home
+android_sdk_home = os.environ['ANDROID_HOME']
+sdkmanger_home = os.path.join(android_sdk_home, 'tools/bin/sdkmanager')
 gradle_home = os.path.join(os.environ['GRADLE_HOME'], 'bin')  # you need to set GRADLE_HOME env in /etc/profile
 download_path = os.path.join(user_home, 'AppAutoBuilder/MySampleDir')  # project download path
+project_path = os.path.join(user_home, 'AppAutoBuilder')
 gradlew_path = os.path.join(download_path, 'gradlew')  # dir of file gradlew
+settings_gradle_path = os.path.join(download_path,'settings.gradle') # path of settings.gradle
+build_log_name = "app.log" # 记录build log的文件名
+build_gradle_path = os.path.join(download_path, 'build.gradle')  # dir of build.gradle of root project
+
+experiment_home = "/home/wqc/experiment/jdk1.8"
+
+wrong_indicator = "* What went wrong:" # 错误指示头
+success_indicator = "BUILD SUCCESSFUL" # 构建成功指示头
+error_end_indicator = "* Try:" # 一般在错误信息结束后会出现的指示头
+
+project_name = "root project"
+task_name = "unknown task" # task名
+
+
+dependency_project = "root project" # 发生依赖问题的项目
+dependency_type = "" # 出现问题的依赖类别，包括test compile等
+dependency_group = "" # 依赖的group
+dependency_name = "" # 依赖的name
+dependency_version = "" # 依赖的版本号
+dependency_file = "" #依赖的具体文件名 如shadow.jar
+
+plugin_id = "" #插件的id， 例如 Plugin with id 'nexus' not found.
+plugin_version = "" #插件的版本号
+plugin_class = "" #插件所在的class
+
+property_name = "" #属性名
+property_class = "" #构建时会在指定路径寻找property，本变量指示该路经，如org.gradle.api.plugins.quality.CheckstyleExtension_Decorated@6815a68d
+property_project = "root project" #属性出现在哪个project中
+property_list = []
+
+report_file_path = "" # 有一种log在发生test失败后会给出报错的文件路径
+
+build_tools_revision = "" # 关于sdk的错误，log会给出build tool的版本：failed to find Build Tools revision 19.0.3
+platform_version = "" #本地找不到的sdk版本名
+ndk_version = "" # 本地找不到的ndk版本
+
+artifact_group = "" # 工件
+artifact_name = ""
+artifact_version = ""
+artifact = ""
+
+build_file_path = "" #构建文件如build.gradle文件的路径
+
+conflict_module1 = "" #出现dependency问题时可能会报错：两个模块冲突，需要记录两个模块的名字
+conflict_module2 = "" #例如A conflict was found between the following modules：
+
+repository_name = "" #仓库的名字
+
+project_list = [] # 当项目中有多个子项目时，保存这些子项目的名称
+
+
--- main.py
+++ main.py
@@ -5,74 +5,149 @@ from git import Repo
 import config
 import os
 import sys
-
+import urllib.request
+import errorHandler
+import errorMsgExtraction
 
 
 def chooseIfReplaceProject(url):
+    "询问用户是否用新url中的项目替换现有项目"
     while (True):
         changeflg = input("please input 'yes' or 'no': ")
-        print(changeflg)
         if (changeflg != 'yes' and changeflg != 'no'):
             print("the input is wrong!")
             continue
         elif (changeflg == "yes"):
+            os.system('rm -rf ./MySampleDir')
+            print("start downloading")
             Repo.clone_from(url, to_path=config.download_path)  # clone project from github
+            print("download success")
             break
         else:
             print("using the existing project as MySampleDir")
             break
 
+def checkIfUrlEmpty():
+    "检查用户输入的url是否为空"
+    while True:
+        try:
+            url = input()  # read url
+            urllib.request.urlopen(url)
+            return url
+        except Exception as err:
+            print(err)
+            print("the input url is invalid, please try again")
+
+def experiment():
+    f = open('/home/wqc/experiment/data/data0509.txt', 'r')
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        url = line[:-1]
+        project = url.split("/")[-1]
+        print(url)
+        os.chdir(config.project_path)
+        os.system("pwd")
+        os.system('rm -rf ./MySampleDir')
+        print("start downloading")
+        Repo.clone_from(url, to_path=config.download_path)  # clone project from github
+        print("download success")
+        os.chdir(config.download_path)  # move to project root directory to execute gradlew
+        if not os.path.exists(config.gradlew_path):  # we don't process this situation
+            print('the project is not complete!')
+            continue
+        else:
+            flg = 1
+            print('file gradlew detected')
+            while True:
+                print("executing gradlew build")
+                file = os.path.join(config.experiment_home, project + str(flg) + '.txt')
+                os.system(config.gradlew_path + ' build > ' + config.build_log_name + ' 2>&1')
+                f = open(config.build_log_name,'r')
+                lines = f.readlines()
+                f.close()
+                f = open(file,'w')
+                for line in lines:
+                    f.write(line)
+                f.close()
+                flg = flg + 1
+                if errorMsgExtraction.checkIfBuildSuccess():  # 执行成功
+                    print("build Succeed!")
+                    apk_list = os.popen("find -name '*.apk'").readlines()  # 查找所有以.apk结束的文件
+                    if len(apk_list) > 0:
+                        print("apk path:")
+                        for apk in apk_list:  # 查找成功
+                            print(os.getcwd() + apk[1:-1])
+                    else:  # 查找失败
+                        print("No Apk Generated!")
+                    break
+                else:  # 执行失败
+                    print("build failed!")
+                    extractionStatus, errorInfo = errorMsgExtraction.errorMessageExtraction()  # 抽取错误信息
+                    keys = list(errorInfo.keys())
+                    values = list(errorInfo.values())
+                    if extractionStatus:  # 抽取成功
+                        print("Extraction Succeed!")
+                        # print(errorInfo)
+                        fixingSuccess = errorHandler.errorHandler(errorInfo)
+                        if not fixingSuccess:  # 修复失败退出
+                            print("Fixing Failed!")
+                            break
+                    else:  # 抽取失败退出
+                        print("Extraction Failed!")
+                        break
+
 
 def main():
     print("It is a App-Auto-Builder from NWPU")
-    print("Please enter the address of the Project!")
-    url = input()  # read url
-    if (not os.path.exists(config.download_path)):
+    url = input("Please enter the address of the Project: ").strip()
+    if not os.path.exists(config.download_path): #项目路径MySampleDir不存在
+        print("start downloading")
         Repo.clone_from(url, to_path=config.download_path)  # clone project from github
+        print("download success")
     else:
         print("current project has MySampleDir!")
         print("do you want to replace old project with the new one?")
         chooseIfReplaceProject(url)
-
     os.chdir(config.download_path)  # move to project root directory to execute gradlew
     print("We have change to the Dir: " + os.getcwd())
 
-    if (not os.path.exists(config.gradlew_path)):  # we dont process this situation
+    if not os.path.exists(config.gradlew_path):  # we don't process this situation
         print('the project is not complete!')
         sys.exit()
     else:
         print('file gradlew detected')
-        print("executing gradlew assemble")
-        # print(gradlew_path)
-        os.system(config.gradlew_path + ' assemble --quiet >app.log 2>&1')
-        
-
-        # settingGradleFile = os.getcwd() + '/settings.gradle' # dir of settings.gradle
-        # if not os.path.exists(settingGradleFile):
-        #    createSettingGradleFile(settingGradleFile)
-        # os.system(gradle_home + '/gradle init')
-        # os.system(gradlew_path + ' build')
-
-
-
-
-
-
-
-
-
-
-def createSettingGradleFile(settingGradleFile):  # create file settings.gradle
-    # print('test1')
-    filename = input('please input the name of project: ')  # get the name of the project to create settings.gradle
-
-    if not os.path.exists(os.getcwd() + '/' + filename):
-        return
-
-    f = open(settingGradleFile, 'w')
-    # print('include ' + '\':' + filename + '\'')
-    f.write('include ' + '\':' + filename + '\'')
-    f.close()
+        # os.system(config.gradlew_path + ' -Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=7890')
+        # os.system(config.gradlew_path + ' -Dhttps.proxyHost=127.0.0.1 -Dhttps.proxyPort=7890')
+        while True:
+            print("executing gradlew build")
+            os.system(config.gradlew_path + ' build > ' + config.build_log_name + ' 2>&1')
+            if errorMsgExtraction.checkIfBuildSuccess():  # 执行成功
+                print("build Succeed!")
+                apk_list = os.popen("find -name '*.apk'").readlines() # 查找所有以.apk结束的文件
+                if len(apk_list) > 0:
+                    print("apk path:")
+                    for apk in apk_list: # 查找成功
+                        print(os.getcwd() + apk[1:-1])
+                else: # 查找失败
+                    print("No Apk Generated!")
+                break
+            else:    # 执行失败
+                print("build failed!")
+                extractionStatus,errorInfo = errorMsgExtraction.errorMessageExtraction() # 抽取错误信息
+                keys = list(errorInfo.keys())
+                values = list(errorInfo.values())
+                if extractionStatus: # 抽取成功
+                    print("Extraction Succeed!")
+                    # print(errorInfo)
+                    fixingSuccess = errorHandler.errorHandler(errorInfo)
+                    if not fixingSuccess: # 修复失败退出
+                        print("Fixing Failed!")
+                        break
+                else: # 抽取失败退出
+                    print("Extraction Failed!")
+                    break
+    # experiment()
 
 if __name__ == "__main__":
     main()
